721c225f8371a5e1c4a07020a34f2895528d70cc
lhel <luishenriqueestrela@gmail.com>
2016-01-30 17:47:32 -0300

    Dockerizing app
    add docker-composer.yml
    Dockerfile:
    	- choose ubuntu as image base
    	- add python3.4 installation
    edit envs file:
    	- add MONGODB_HOST variable
    config.py:
    	- edit acess settings to Mongodb

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
-FROM python:3.4.4-wheezy
+FROM ubuntu:latest
+
+RUN apt-get update && \
+	apt-get install -y python3.4 python3.4-dev python3-pip && \
+	rm -rf /var/lib/apt/lists/*
 
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
 
 COPY requirements.txt /usr/src/app/
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip3 install -r requirements.txt
 
 COPY . /usr/src/app
-
-EXPOSE 5000
-
-CMD ["python", "./manage.py", "runserver", "--host", "0.0.0.0"]
--- config.py
+++ config.py
@@ -10,6 +10,7 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MONGODB_HOST = os.environ.get('MONGODB_HOST')
 
     @staticmethod
     def init_app(app):
@@ -18,18 +19,16 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    MONGO_SETTINGS = {'db': os.environ.get('DEV_DATABASE_URL') or
-                      'dev_lochat_db'}
+    MONGODB_DB = os.environ.get('DEV_DATABASE_URL') or 'dev_lochat_db'
 
 
 class TestingConfig(Config):
     TESTING = True
-    MONGO_SETTINGS = {'db': os.environ.get('TEST_DATABASE_URL') or
-                      'test_lochat_db'}
+    MONGODB_DB = os.environ.get('TEST_DATABASE_URL') or 'test_lochat_db'
 
 
 class ProductionConfig(Config):
-    MONGO_SETTINGS = {'db': os.environ.get('DATABASE_URL')}
+    MONGODB_DB = os.environ.get('DATABASE_URL')
 
 
 config = {
